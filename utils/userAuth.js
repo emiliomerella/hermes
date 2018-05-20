a6f90d929332cd22674dcc4fc307dc269a164be0
emiliomerella <emilio.merella@gmail.com>
2018-05-20 12:44:49 +0200

    add notes
    add some annotations

--- utils/userAuth.js
+++ utils/userAuth.js
@@ -3,10 +3,11 @@ const jwt = require('jsonwebtoken')
 const secret = process.env.AUTH_SECRET
 
 module.exports = (token) => {
+  // here we can authorize users by checking the incoming token.
+  // adapt this to work properly in your arch
   try {
     return jwt.verify(token, secret)
   } catch (e) {
-    // console.error(e);
     return e
   }
 }
