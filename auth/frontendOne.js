296d13c363cb671f48a34013faa88a556729e36e
emiliomerella <emilio.merella@gmail.com>
2018-05-20 12:50:52 +0200

    add some notes

--- auth/frontendOne.js
+++ auth/frontendOne.js
@@ -2,24 +2,28 @@ const config = require('../config').knownClients.frontendOne
 const { userAuth } = require('../utils')
 
 module.exports = (socket, next) => {
-  const handshake = {
-    userAgent: socket.handshake.headers['user-agent'],
-    realIp: socket.handshake.headers['x-real-ip'],
-    fwd4Ip: socket.handshake.headers['x-forwarded-for'],
-    client: socket.handshake.headers['x-client'],
-    token: socket.handshake.query.token,
-  }
-  // console.log(handshake);
-
-  if (handshake.client !== config.id) {
+  if (socket.handshake.headers['x-client'] !== config.id) {
     return next(new Error('unknown client'))
   }
 
-  const response = userAuth(handshake.token)
+  // check token
+  const response = userAuth(socket.handshake.query.token)
 
   if (response instanceof Error) {
     return next(new Error(response.message))
   }
 
-  next()
+  // create an object like the following to store access informations into your DB
+  // const data = {
+  //   userAgent: socket.handshake.headers['user-agent'],
+  //   realIp: socket.handshake.headers['x-real-ip'],
+  //   fwd4Ip: socket.handshake.headers['x-forwarded-for'],
+  //   client: socket.handshake.headers['x-client'],
+  //   token: socket.handshake.query.token,
+  //   user: response,
+  // }
+
+  // add user to socket
+  socket.user = response
+  return next()
 }
