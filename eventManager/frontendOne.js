ec9f631d8963bd7a03f51641873ed07401329216
emiliomerella <emilio.merella@gmail.com>
2018-05-20 12:49:55 +0200

    use validator middleware

--- eventManager/frontendOne.js
+++ eventManager/frontendOne.js
@@ -1,8 +1,17 @@
-module.exports = (ns) => {
+const { validator } = require('../middlewares')
 
+module.exports = (ns) => {
   ns.on('connect', (socket) => {
-    console.log(`[ INFO ] - frontendOne - client connected`)
+    if (socket.user) {
+      // TODO: create a room
+    } else {
+      return
+    }
+
+    console.log(`[ INFO ] - frontendOne - client ${socket.user.id} connected`);
     socket.emit('welcome', 'welcome to "frontendOne" namespace')
+
+    socket.use(validator)
 
     socket.on('new-message', data => ns.emit('update:new-message', data))
 
